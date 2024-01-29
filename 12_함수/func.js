2ece3dc9e6961c44eafe0eb849d8639ff278ba6d
ohgus <anakin1108@naver.com>
2024-01-29 21:08:04 +0900

    [docs] study chapter_12.7

--- 12_함수/func.js
+++ 12_함수/func.js
@@ -1,12 +1,19 @@
-function changeValue(primitive, obj) {
-  primitive += 100; // 원시 값은 불변 값이기 때문에 재할당으로 값을 변경
-  obj.name = "lee";
+// function changeValue(primitive, obj) {
+//   primitive += 100; // 원시 값은 불변 값이기 때문에 재할당으로 값을 변경
+//   obj.name = "lee";
+// }
+
+// let num = 100;
+// const person = { name: "oh" };
+
+// changeValue(num, person);
+
+// console.log(num); // 100 -> 원시 값은 원본 값이 훼손되지 않는다.
+// console.log(person); // name: "lee" -> 객체는 원본 값이 훼손된다.
+
+function factorial(n) {
+  if (n <= 1) return 1;
+  return n * factorial(n - 1);
 }
 
-let num = 100;
-const person = { name: "oh" };
-
-changeValue(num, person);
-
-console.log(num); // 100 -> 원시 값은 원본 값이 훼손되지 않는다.
-console.log(person); // name: "lee" -> 객체는 원본 값이 훼손된다.
+console.log(factorial(5));
